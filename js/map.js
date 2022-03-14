345d82349a4880bea972feff155235aa2104a11e
jobkaeHenry <1zzangjun@gmail.com>
2022-03-14 17:29:15 +0900

    ㅠㅁ차

--- js/map.js
+++ js/map.js
@@ -8,15 +8,15 @@ var map = new kakao.maps.Map(mapContainer, mapOption); // 지도를 생성합니
 
 // 마커를 표시할 위치와 title 객체 배열입니다 
 var positions = [{
-    title: '인천공항',
+    title: '인천공항 체험장',
     latlng: new kakao.maps.LatLng(37.44922252207683, 126.4515353189167)
   },
   {
-    title: '김포공항',
+    title: '김포공항 체험장',
     latlng: new kakao.maps.LatLng(37.559921437601524, 126.80246682790663)
   },
   {
-    title: '제주공항',
+    title: '제주공항 체험장',
     latlng: new kakao.maps.LatLng(33.50726107097914, 126.49331789961546)
   },
 ];
@@ -38,44 +38,7 @@ for (var i = 0; i < positions.length; i++) {
     image: markerImage // 마커 이미지 
   });
 
-  // 커스텀 오버레이에 표시할 컨텐츠 입니다
-  // 커스텀 오버레이는 아래와 같이 사용자가 자유롭게 컨텐츠를 구성하고 이벤트를 제어할 수 있기 때문에
-  // 별도의 이벤트 메소드를 제공하지 않습니다 
-  var content = '<div class="wrap">' +
-    '    <div class="info">' +
-    '        <div class="title">' +
-    '            에어버스 시승장' +
-    '            <div class="close" onclick="closeOverlay()" title="닫기"></div>' +
-    '        </div>' +
-    '        <div class="body">' +
-    '            <div class="logo">' +
-    '                <img src="../img/airbus_logo.svg" width="73" height="70" max-width: "65px" background-color="#fff">' +
-    '           </div>' +
-    '            <div class="desc">' +
-    '                <div class="ellipsis">에어버스 헬리콥터를 체험해보세요' +
-    '                <div class="jibun ellipsis">Experience Airbus Helicopter</div>' +
-    '            </div>' +
-    '        </div>' +
-    '    </div>' +
-    '</div>';
-
-  // 마커 위에 커스텀오버레이를 표시합니다
-  // 마커를 중심으로 커스텀 오버레이를 표시하기위해 CSS를 이용해 위치를 설정했습니다
-  var overlay = new kakao.maps.CustomOverlay({
-    content: content,
-    map: map,
-    position: marker.getPosition()
-  });
-
-  // 마커를 클릭했을 때 커스텀 오버레이를 표시합니다
-  kakao.maps.event.addListener(marker, 'click', function () {
-    overlay.setMap(map);
-  });
-
-  // 커스텀 오버레이를 닫기 위해 호출되는 함수입니다 
-  function closeOverlay() {
-    overlay.setMap(null);
-  }
+  
 
 }
 // 지도 확대 축소를 제어할 수 있는  줌 컨트롤을 생성합니다
