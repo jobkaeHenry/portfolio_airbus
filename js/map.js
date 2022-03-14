7eebf048ea319ca05b08ab6c84a580a414030d5f
jobkaeHenry <1zzangjun@gmail.com>
2022-03-14 15:55:04 +0900

    ss

--- js/map.js
+++ js/map.js
@@ -1,7 +1,23 @@
-var container = document.getElementById('map'); //지도를 담을 영역의 DOM 레퍼런스
-var options = { //지도를 생성할 때 필요한 기본 옵션
-	center: new kakao.maps.LatLng(33.450701, 126.570667), //지도의 중심좌표.
-	level: 3 //지도의 레벨(확대, 축소 정도)
-};
+var mapContainer = document.getElementById('map'), // 지도를 표시할 div  
+    mapOption = { 
+        center: new kakao.maps.LatLng(36.29547788128749, 127.69572491745187), // 지도의 중심좌표
+        level: 1 // 지도의 확대 레벨
+    };
 
-var map = new kakao.maps.Map(container, options); //지도 생성 및 객체 리턴
+var map = new kakao.maps.Map(mapContainer, mapOption); // 지도를 생성합니다
+ 
+// 마커를 표시할 위치와 title 객체 배열입니다 
+var positions = [
+    {
+        title: '인천공항', 
+        latlng: new kakao.maps.LatLng(37.44922252207683, 126.4515353189167)
+    },
+    {
+        title: '김포공항', 
+        latlng: new kakao.maps.LatLng(37.559921437601524, 126.80246682790663)
+    },
+    {
+        title: '제주공항', 
+        latlng: new kakao.maps.LatLng(33.50726107097914, 126.49331789961546)
+    },
+];
